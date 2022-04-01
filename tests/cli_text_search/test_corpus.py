a79d81004ed9d86c4c59498bf01d103a07cfdb16
marc <marc.wagner.lu@gmail.com>
2022-04-01 03:04:19 +0200

    add TestCorpus

--- tests/cli_text_search/test_corpus.py
+++ tests/cli_text_search/test_corpus.py
@@ -2,36 +2,42 @@
 unit tests for Corpus class
 """
 import numpy as np
+from scipy.sparse import csr_matrix
 
 from cli_text_search.corpus import Corpus
 
 
-def test_get_document_term_matrix():
-    assert False
+class TestCorpus:
+
+    corpus = Corpus(["one one one two three four five"])
 
 
-def test_get_dictionary():
-    assert False
+    def test_get_document_term_matrix(self):
+        assert False
 
 
-def test_get_tokens_in_document():
-    assert False
+    def test_get_dictionary(self):
+        corpus_dict = self.corpus.get_dictionary()
+        assert "one" in corpus_dict
+        assert "zero" not in corpus_dict
+        assert len(corpus_dict) == 5
 
 
-def test_get_best_match():
-    assert False
+    def test_get_tokens_in_document(self):
+        assert False
 
 
-def test_apply_floor_zero():
+    def test_get_best_match(self):
+        assert False
 
-    input_array = np.ndarray(shape=(1, 3),
-                             buffer=np.array([0, -1, 1]),
-                             dtype=int)
 
-    expected_array = np.ndarray(shape=(1, 3),
-                                buffer=np.array([0, 0, 1]),
-                                dtype=int)
+    def test_apply_floor_zero(self):
 
-    result_array = Corpus.apply_floor_zero(input_array)
+        input_csr = csr_matrix(np.array([0, -1, 1]))
+        expected_csr = csr_matrix(np.array([0, 0, 1]))
 
-    assert result_array == expected_array
+        result_csr = Corpus.apply_floor_zero(input_csr)
+
+        assert result_csr[0, 1] == expected_csr[0, 1]
+        assert result_csr[0, 0] == expected_csr[0, 0]
+        assert result_csr[0, 2] == expected_csr[0, 2]
