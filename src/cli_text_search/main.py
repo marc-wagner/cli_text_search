ff669b6df39a607967937042ede9d9b73d8cc677
marc <marc.wagner.lu@gmail.com>
2022-04-05 00:21:37 +0200

    WIP init on remote

--- src/cli_text_search/main.py
+++ src/cli_text_search/main.py
@@ -1,6 +1,11 @@
+import asyncio
+import json
 import logging
 import os
 import sys
+import requests
+import http
+
 from prompt_toolkit import PromptSession
 
 from cli_text_search.corpus import Corpus
@@ -10,8 +15,10 @@ logging.basicConfig(level="DEBUG",
                     stream=sys.stdout,
                     format=logformat,
                     datefmt="%Y-%m-%d %H:%M:%S")
+http.client.HTTPConnection.debuglevel = 1
 
 max_results = 10  # max number of results returned
+nr_workers = 1
 
 
 # ---- Python API ----
@@ -40,7 +47,7 @@ def collect_file_paths(directory):
     return text_file_paths
 
 
-def search(answer, corpus):
+async def search(answer, corpus):
     """search for string in all documents in corpus.
     tokenize search term and score documents based on presence of each search word
 
@@ -62,30 +69,58 @@ def search(answer, corpus):
     return output
 
 
-def invoke_prompt(folder_path):
+# TODO move this to ProxyCorpus. figure out how to best handle worker config: embed in class?
+async def init_remote_corpus(input, input_type, worker):
+    logging.debug(f"worker: {worker}")
+    route = '/init'
+    headers = {'Content-type': 'application/json'}
+    url = os.environ.get(f"WORKER_HOST_{worker}") + ":" + os.environ.get(f"WORKER_PORT_{worker}") + route
+    body = json.dumps({"file_paths": input})
+    logging.debug(f"sending http POST request to worker {worker} at {url}")
+    res = requests.post(url, body, headers=headers)
+    res.raise_for_status()
+    logging.debug(f"received http response from {url} with status code {res.status_code}")
+    return res.json()
+
+
+async def build_corpus(folder_path, big):
+    logging.debug(f"input path: {folder_path}")
+    file_paths = collect_file_paths(folder_path)
+    logging.debug(f"len filepaths: {len(file_paths)}")
+    if len(file_paths) == 0:
+        raise FileNotFoundError(f"no text files found in directory {folder_path}")
+    if big:
+        corpus = []
+        # split files into chunks
+        chunk_size = int(round(len(file_paths) / nr_workers, 0))
+        # res = await asyncio.gather(*(corpus.append(init_remote_corpus(input=file_paths[(i-1)*chunk_size: i*chunk_size-1],
+        #                                                               input_type="filename",
+        #                                                               worker=i)) for i in range(2)))
+        await asyncio.gather(*(init_remote_corpus(input=file_paths[(i - 1) * chunk_size: i * chunk_size:],
+                                                  input_type="filename",
+                                                  worker=i) for i in range(nr_workers)))
+    else:
+        corpus = [Corpus(file_paths, input_type="filename")]
+    return corpus
+
+
+async def invoke_prompt(folder_path, big=False):
     """manage program workflow and prompt:
         first load corpus of documents,
         then loop: prompt for search term, show matching documents
         until the user types 'quit'
 
         :param folder_path: directory that contains documents to search in
+        :param big: launch search on a distributed system
         :returns: None
     """
-    logging.debug(f"input path: {folder_path}")
-
-    file_paths = collect_file_paths(folder_path)
-    print(f"len filepaths: {len(file_paths)}")
-    if len(file_paths) == 0:
-        raise FileNotFoundError(f"no text files found in directory {folder_path}")
-    # TODO make this asynch
-    corpus = Corpus(file_paths, input_type="filename")
+    corpus = await build_corpus(folder_path, big)
 
     loop = True
     logging.debug(f"starting interactive user prompt")
     s = PromptSession(message="'search (type 'quit' to exit) > ")
     while loop:
         answer = s.prompt()
-        # answer = "hello world"
         try:
             if answer == "quit":
                 logging.info(f"user requested to quit program execution")
@@ -97,13 +132,14 @@ def invoke_prompt(folder_path):
 
 
 # ---- CLI ----
-def main(argv):
-    f = argv[1] if len(argv) > 1 else None
+async def main(argv):
+    f = argv[len(argv)-1] if len(argv) > 1 else None
     if f:
-        invoke_prompt(f)
+        big = (argv[1] == '--big')
+        await invoke_prompt(f, big)
     else:
         raise Exception('Missing parameter: folder_path')
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    asyncio.run(main(sys.argv))
